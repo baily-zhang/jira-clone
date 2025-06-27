80e7063a51766f8fbcdbed669e1b116cf05380d5
Baily <blitheshuo@gmail.com>
2025-06-26 23:11:25 -0400

    test dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
-import { Select } from "react-day-picker";
+import { Dropdown, Select } from "react-day-picker";
 
 export default function Home() {
   return (
@@ -11,8 +11,11 @@ export default function Home() {
       <Button variant="ghost">Ghost</Button>
       <Button variant="muted">sadfMuted</Button>
       <Button variant="teritrary" >Teritrary</Button>
+
       <Input />
       <Select />
+      <Dropdown >
+      </Dropdown >
     </div >
 
   );
